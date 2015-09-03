d9cda9845c9761f5659f2e0b643383cf5b7be0b3
bergi <bergi@axolotlfarm.org>
2015-09-03 19:35:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rdf-serializer-abstract
+
+Abstract base class for RDF-Interfaces serializer implementations.
---
+++ index.js
@@ -0,0 +1,57 @@
+var util = require('util')
+var Readable = require('stream').Readable
+
+function AbstractSerializer (rdf) {
+  this.rdf = rdf
+}
+
+AbstractSerializer.prototype.stream = function (inputStream, base, filter) {
+  var outputStream = new AbstractSerializer.DataReadStream()
+
+  AbstractSerializer.streamToGraph(inputStream, this.rdf.createGraph()).then(function (graph) {
+    self.serialize(graph, function (error, data) {
+      if (error) {
+        outputStream.emit('error', error)
+      } else {
+        outputStream.push(data)
+        outputStream.emit('end')
+      }
+    })
+  }).catch(function (error) {
+    outputStream.emit('error', error)
+  })
+
+  return outputStream
+}
+
+AbstractSerializer.streamToGraph = function (stream, graph) {
+  return new Promise(function (resolve, reject) {
+    if (typeof stream !== 'object' || typeof stream.read !== 'function') {
+      return resolve(stream)
+    }
+
+    stream.on('data', function (triple) {
+      graph.add(triple)
+    })
+
+    stream.on('end', function () {
+      resolve(graph)
+    })
+
+    stream.on('error', function (error) {
+      reject(error)
+    })
+  })
+}
+
+AbstractSerializer.DataReadStream = function () {
+  Readable.call(this, {objectMode: true})
+
+  this._read = function () {
+    return 0
+  }
+}
+
+util.inherits(AbstractSerializer.DataReadStream, Readable)
+
+module.exports = AbstractSerializer
